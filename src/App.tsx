4c0fe8e6f3ae992fe4105f55e20603a75457f787
Raul Berari <raulberari@protonmail.com>
2024-11-26 18:01:59 +0200

    Made the table wider

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
         className="flex justify-center flex-auto relative"
         // style={{ background: "linear-gradient(#2c5363,#0f2027)" }}
       >
-        <div className="w-full max-w-[80%] flex flex-col">
+        <div className="w-full flex flex-col">
           <header className="border-b-2 px-3 border-gray-200 flex justify-between">
             <button
               className="mr-3 text-xl"
